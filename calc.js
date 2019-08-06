393a1e54254a38e82f2f3b7fb816448b6b6c9602
Jorge Cruz <jlcruz7125@gmail.com>
2019-08-05 21:54:10 -0400

    Added the basic

--- calc.js
+++ calc.js
@@ -1,12 +1,37 @@
-function add(num1, num2){
+const all = document.querySelectorAll('button');
+all.forEach(key=>{
+    key.classList.add('keys');
+})
 
+function add(num1, num2){
+    return num1 + num2;
 }
 function sub(num1, num2){
-    
+    return num1 - num2;
 }
 function mult(num1, num2){
-    
+    return num1 * num2;   
 }
 function div(num1, num2){
-    
+    if(num2 != 0){
+        return num1 / num2;
+    }
+    else{
+        return "Cannot Divide by Zero"
+    }
+}
+
+function operate(operator, num1, num2){
+    if(operator == "+"){
+        add(num1, num2);
+    }
+    else if(operator == "-"){
+        sub(num1, num2);
+    }
+    else if(operator == "*"){
+        mult(num1, num2);
+    }
+    else if(operator == "/"){
+        div(num1, num2);
+    }
 }
